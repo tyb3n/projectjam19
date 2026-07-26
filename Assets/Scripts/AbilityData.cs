710e5efce310362b4a41e2a18b5f94dd485139b9
Adrien Sesé <a.sese.pro@gmail.com>
2026-07-26 14:41:41 +0200

    [PROG] Add Ability ScriptableObject and assets

---
+++ Assets/Scripts/AbilityData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AbilityData", menuName = "Scriptable Objects/Ability")]
+public class AbilityData : ScriptableObject
+{
+    public string abilityName;
+}
